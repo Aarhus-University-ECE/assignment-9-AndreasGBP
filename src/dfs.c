d360cb3b6e89038e2a475a44eb5e9f1d0d66a126
AndreasGBP <114382655+AndreasGBP@users.noreply.github.com>
2022-11-07 17:35:23 +0100

    Final edit

--- src/dfs.c
+++ src/dfs.c
@@ -12,40 +12,97 @@ void DFT (node * root)
 {
 	// Implement DFS
 	// Hint: You can use print_node, print_tree and/or print_stack.
+
+  printf("tree: \n");
+  print_tree(root, 0);
+  printf("\n depth first search with stack: \n");
+
+  // This function creates the stack
+  stack *stack = malloc(sizeof(stack));
+  node *temp = root;
+
+  //This function pushes the root node
+  printf("%d, ", root->num);
+  stack = push(stack, root);
+  temp -> visited = true;
+  bool done = false; 
+
+  while (!done) 
+  {
+    while(temp -> lchild !=NULL) //Keeps moving thorugh the elements to the left as far as possible, and notes them as marked and "visited"
+    {
+      temp = temp -> lchild;
+      printf("%d, ,", temp -> num); 
+      stack = push(stack, temp); 
+
+      temp -> visited = true; // Visited notes if the node has been "visited" and marked as a leafnode by parent
+    } 
+  // Checks if the nodes has any child nodes left to examine. If not its finds own parent node, and temporarily points to it. 
+   while ((temp -> lchild == NULL || temp -> lchild -> visited == true) && (temp -> rchild == NULL || temp -> rchild -> visited == true))
+   {
+    if (stack -> next -> node == root && root -> rchild -> visited == true)
+    {
+      done = true;
+    }
+    stack = pop(stack);
+    temp = stack -> node; 
+   
+   if (done)
+   {
+    break; 
+   }
+  }
+  if (done)
+  {
+    break;
+  }
+
+  temp = temp -> rchild;
+  printf("%d, ", temp -> num);
+  stack = push(stack, temp);
+  temp -> visited  = true;
+}
+  printf("\n"); 
 }
 
 node *make_node (int num, node * left, node * right)
 {
-	return 0;
+	 node *treePointer = malloc(sizeof(node));
+
+  //Node with num, left & right child and wether or not it has been "visited"
+  (treePointer) -> num = num;
+  (treePointer) -> visited = false;
+  (treePointer) -> lchild = left;
+  (treePointer) -> rchild = right; 
+
+  return treePointer;  
 }
 
-void free_node (node * p)
+void free_node (node * p) 
 {
-	
+	free(p);
 }
 
 
 void print_node (node * p)
 {
-
   if (p == NULL)
-    printf ("NULL\n");
-  else
-    printf ("%d", p->num);
+    printf("NULL\n");
+  else 
+    printf("%d", p -> num);
 }
 
 
 void print_tree (node * p, int depth)
 {
-  for (int i = 0; i < depth; i = i + 1)
-    printf (" ");
+  for (int i = 0; i < depth; i = i + 1);
+  printf (" ");
   printf ("| ");
-
   if (p == NULL)
     printf ("NULL\n");
   else
     printf ("[%d]\n", p->num);
-
+    
 
   if (p->lchild)
     {
@@ -53,17 +110,21 @@ void print_tree (node * p, int depth)
     }
 
   if (p->rchild)
-    print_tree (p->rchild, depth + 1);
+    print_tree (p->rchild, depth + 1); 
 }
 
 stack *push (stack * topp, node * node)
 {
-	return 0;
+	stack *t = malloc(sizeof(stack)); //Create new stack node 
+
+  t -> next = topp; //Add the tree node to add
+  t -> node = node; //Add previous topp node
+  return t;   
 }
 
 bool isEmpty (stack * topp)
 {
-  return false;
+  return false; 
 }
 
 node *top (stack * topp)
@@ -73,10 +134,15 @@ node *top (stack * topp)
 
 // Utility function to pop topp  
 // element from the stack 
-
-stack *pop (stack * topp)
+// Pops first node in stack. Returns node after
+// returned pointer points to parrent node. 
+stack *pop (stack * topp) 
 {
-	return 0;
+  stack *temp = topp;
+  topp = topp -> next;
+  free (temp);
+
+  return topp;
 }
 
 void print_stack (stack * topp)
--- src/search.c
+++ src/search.c
@@ -1,7 +1,18 @@
 #include "search.h"
 #include <stdbool.h>
 
-bool search(int a[], int n, int x)
+bool search(int a[], int n, int x) //Finding integer "x" in array "a" with the length "n"
 {
-    return 0;
+    if(n > 0 && a[n-1] == x) //The base case
+    {
+        return true;
+    }
+    else if (n>0 && a[n-1] != x) //The recursive step
+    {
+        return search(a, n-1, x);
+    }
+    else if (n == 0) //The base case 
+    {
+        return false;
+    }
 }
